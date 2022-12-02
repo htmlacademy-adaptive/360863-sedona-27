fc648335b5c12e61c7be957f8ee370c2b1c7fb70
Leonid <alraun_work@mail.ru>
2022-12-02 22:27:09 +0300

    подправил стилизацию

--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,6 @@
   const hamburgerSecondLine = document.querySelector('.main-nav__toggle-line-second');
   const hamburgerThirdLine = document.querySelector('.main-nav__toggle-line-third');
   const menu = document.querySelector('.main-nav__list');
-
   if (hamburger) {
     hamburger.addEventListener('click', () => {
       menu.classList.toggle('main-nav__list--shown');
