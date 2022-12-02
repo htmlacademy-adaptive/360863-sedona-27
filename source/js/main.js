284a7a8fd5bfe4ef9f9e7526270f6b827499a4f9
Leonid <alraun_work@mail.ru>
2022-12-02 21:29:41 +0300

    главная страница

---
+++ source/js/main.js
@@ -0,0 +1,14 @@
+  const hamburger = document.querySelector('.main-nav__toggle');
+  const hamburgerSecondLine = document.querySelector('.main-nav__toggle-line-second');
+  const hamburgerThirdLine = document.querySelector('.main-nav__toggle-line-third');
+  const menu = document.querySelector('.main-nav__list');
+
+  if (hamburger) {
+    hamburger.addEventListener('click', () => {
+      menu.classList.toggle('main-nav__list--shown');
+      hamburger.classList.toggle('active');
+      hamburgerSecondLine.classList.toggle('main-nav__toggle-line--light');
+      hamburgerThirdLine.classList.toggle('main-nav__toggle-line--light');
+    });
+  }
+
