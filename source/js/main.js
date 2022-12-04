1a86b61e8505b6a5194ff2dd8aea01b470bcdfe2
Leonid <alraun_work@mail.ru>
2022-12-04 20:51:01 +0300

    страница с формой

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,15 @@
   const hamburger = document.querySelector('.main-nav__toggle');
-  const hamburgerSecondLine = document.querySelector('.main-nav__toggle-line-second');
-  const hamburgerThirdLine = document.querySelector('.main-nav__toggle-line-third');
   const menu = document.querySelector('.main-nav__list');
+  const hamburgerThirdLines = document.querySelectorAll('.main-nav__toggle-line');
+
   if (hamburger) {
     hamburger.addEventListener('click', () => {
       menu.classList.toggle('main-nav__list--shown');
       hamburger.classList.toggle('active');
-      hamburgerSecondLine.classList.toggle('main-nav__toggle-line--light');
-      hamburgerThirdLine.classList.toggle('main-nav__toggle-line--light');
+
+      hamburgerThirdLines.forEach( (line) => {
+        line.classList.toggle('main-nav__toggle-line--light');
+      } );
     });
   }
+
