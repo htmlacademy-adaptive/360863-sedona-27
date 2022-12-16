66770c6faa55a1575020043ba7b4fa6af43b593e
Keks <keks@htmlacademy.ru>
2022-12-16 13:43:34 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/main.js
+++ 5/js/main.js
@@ -1,6 +1,10 @@
   const hamburger = document.querySelector('.main-nav__toggle');
   const menu = document.querySelector('.main-nav__list');
+  const header = document.querySelector('.main-header')
   menu.classList.remove('main-nav__list--shown');
+  menu.classList.remove('main-nav__list--nojs');
+  header.classList.remove('main-header--nojs');
+  hamburger.classList.remove('main-nav__toggle--nojs');
 
   if (hamburger) {
     hamburger.addEventListener('click', () => {
